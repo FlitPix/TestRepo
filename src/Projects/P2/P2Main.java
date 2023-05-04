eeb63558686dddd1f13e1ee698873091e7116160
FlitPix <8645405+FlitPix@users.noreply.github.com>
2023-05-04 12:03:08 -0400

    Project 2

---
+++ src/Projects/P2/P2Main.java
@@ -0,0 +1,8 @@
+package Projects.P2;
+
+public class P2Main {
+    public static void main(String[] args){
+        PokemonSelection newGame = new PokemonSelection();
+        newGame.assignPokemon();
+    }
+}
